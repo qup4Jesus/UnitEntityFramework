e8508d8f648a0e45a3c59bff678a0e5b7ba2d346
qup4Jesus <remret1654@gmail.com>
2025-10-11 14:14:25 +0300

    Commit №3 Добавляем функционал в UnitEntityFramework/BLL/Managment/ManagerCompany для управления репозиторием.

--- UnitEntityFramework/BLL/Management/ManagerCompany.cs
+++ UnitEntityFramework/BLL/Management/ManagerCompany.cs
@@ -1,33 +1,72 @@
 ﻿
 using UnitEntityFramework.DAL.Model;
+using UnitEntityFramework.DAL.Repositories;
 
 namespace UnitEntityFramework.BLL.Management
 {
     internal class ManagerCompany: IManager<Company>
     {
-        public void Add(string name, string value)
+        private CompanyRepository _manager;
+
+        public ManagerCompany(CompanyRepository repository)
         {
-            throw new NotImplementedException();
+            _manager = repository;
+        }
+
+        public void Add(string name, string value = "0")
+        {
+            if (String.IsNullOrEmpty(name))
+                throw new ArgumentNullException();
+
+            List<Company> newCompany = new List<Company> { new Company { Name = name } };
+
+            _manager.Add(newCompany);
         }
 
         public void Delete(int id)
         {
-            throw new NotImplementedException();
+            var company = _manager.ReadOne(id);
+
+            if (company is null)
+                throw new ArgumentNullException();
+
+            _manager.Delete(id);
         }
 
         public List<Company> ReadAll()
         {
-            throw new NotImplementedException();
+            var company = _manager.ReadAll();
+
+            if (company is null)
+                throw new ArgumentNullException();
+
+            return company;
         }
 
         public Company ReadOne(int id)
         {
-            throw new NotImplementedException();
+            var company = _manager.ReadOne(id);
+
+            if (company is null)
+                throw new ArgumentNullException();
+
+            return company;
         }
 
         public void Update(int id, string nameColumn, string value)
         {
-            throw new NotImplementedException();
+            var company = _manager.ReadOne(id);
+
+            if (company is null) 
+                throw new ArgumentNullException();
+            if (String.IsNullOrEmpty(nameColumn))
+                throw new ArgumentNullException();
+            if (nameColumn != nameof(company.Name))
+                throw new ArgumentNullException();
+            if (String.IsNullOrEmpty(value))
+                throw new ArgumentNullException();
+
+            _manager.Update(id, nameColumn, value);
         }
     }
 }
