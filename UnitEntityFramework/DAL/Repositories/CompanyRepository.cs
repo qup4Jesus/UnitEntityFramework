e19062f9e0948aa34483cc2a90bb23ee883729f3
qup4Jesus <remret1654@gmail.com>
2025-10-11 13:43:18 +0300

    Commit №1 : Данный комит несет в себе изменения в UnitEntityFramework/DAL/Repositories/CompanyRepository | Создание функционала для работы с базой данных, реализация CRUD.

--- UnitEntityFramework/DAL/Repositories/CompanyRepository.cs
+++ UnitEntityFramework/DAL/Repositories/CompanyRepository.cs
@@ -11,6 +11,8 @@ namespace UnitEntityFramework.DAL.Repositories
         {
             using (_db)
             {
+                // Добавление одной компании, для добавление нескольких
+                // компаний используем AddRange(element1, ... ,element54);
                 _db.Add(newElements);
 
                 _db.SaveChanges();
@@ -19,22 +21,58 @@ namespace UnitEntityFramework.DAL.Repositories
 
         public override void Delete(int id)
         {
-            throw new NotImplementedException();
+            using (_db)
+            {
+                // Удаление конкретной компании по Id
+                var company = _db.Companies.Find(id);
+
+                // Удаление одной компании для удаления нескольких
+                // компаний используем RemoveRange(element1, ... ,element72);
+                _db.Companies.Remove(company);
+
+                _db.SaveChanges();
+            }
         }
 
         public override List<Company> ReadAll()
         {
-            throw new NotImplementedException();
+            using (_db)
+            {
+                // Получение всех существующих компаний в БД
+                return _db.Companies.ToList();
+            }
         }
 
         public override Company ReadOne(int id)
         {
-            throw new NotImplementedException();
+            using (_db)
+            {
+                // Конкретной компании по Id
+                var company = _db.Companies.Find(id);
+
+                return company;
+            }
+
         }
 
         public override void Update(int id, string nameColumn, string value)
         {
-            throw new NotImplementedException();
+            using (_db)
+            {
+                // Изменение компании по ее Id
+                var company = _db.Companies.Find(id);
+
+                switch (nameColumn)
+                {
+                    case nameof(company.Name):
+                        company.Name = value;
+                        break;
+                    default:
+                        throw new Exception();
+                }
+
+                _db.SaveChanges();
+            }
         }
     }
 }
